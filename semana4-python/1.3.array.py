7ce9ec2e807e79e3f1c48693ce6b47513e78abe6
Willian Brandão <willianramos.wrk@gmail.com>
2023-11-19 12:46:06 -0300

    criado um código para ler os elementos de um array de strings

---
+++ semana4-python/1.3.array.py
@@ -0,0 +1,6 @@
+lista_produtos = ['máscaras faciais', 'batons', 'esmaltes', 
+                  'perfumes', 'loções', 'xampus', 'sabonetes', 'delineadores'] 
+
+
+for i in range(len(lista_produtos)):
+    print("Temos {} à venda".format(lista_produtos[i]))
