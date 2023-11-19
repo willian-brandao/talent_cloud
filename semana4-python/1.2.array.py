4985ae8b0c5bca134bf2442484fbbcc748b697aa
Willian Brandão <willianramos.wrk@gmail.com>
2023-11-19 10:17:58 -0300

    criado um programa que imprime a quantidade de elementos de um array e imprime elementos pelo número do índice

---
+++ semana4-python/1.2.array.py
@@ -0,0 +1,11 @@
+lista_musicos = [ 'Djavan', 'Roberto Carlos', 'Elis Regina', 'Tom Jobim', 
+                 'Milton Nascimento', 'Chico Buarque', 'Nara Leão', 'Pitty', 
+                 'Simonal', 'Moacir Santos', 'Caetano Veloso', 'Elza Soares',
+                 'Paulinho da Viola', 'Yamandú Costa', 'Gal Costa'] 
+
+quantidade_de_musicos = len(lista_musicos)
+print("A quantidade toda de músicos na lista é:",quantidade_de_musicos)
+
+print("O músico que está associado ao índice: ",lista_musicos[2])
+print("O músico que está associado ao índice: ",lista_musicos[9])
+print("O músico que está associado ao índice: ",lista_musicos[14])
