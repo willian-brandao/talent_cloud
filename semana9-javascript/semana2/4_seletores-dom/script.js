740de356e7c015d53d905f94894728b920b498ca
Willian Brandão <willianramos.wrk@gmail.com>
2024-01-11 08:40:06 -0300

    Criado arquivo javascript para acessar os elementos da pagina html

---
+++ semana9-javascript/semana2/4_seletores-dom/script.js
@@ -0,0 +1,17 @@
+const titulo = document.getElementById("titulo");
+
+console.log(titulo);
+
+const textos = 
+document.getElementsByClassName("texto-simples");
+console.log(textos[0]);
+
+const segundoTitulo =
+document.querySelector("div h2");
+
+console.log(segundoTitulo);
+
+const textoPorClasse =
+document.querySelectorAll(".texto-simples");
+
+console.log(textoPorClasse);
